0282040b03ebccb1e40ca7b064ef2aa9040b3e54
uhrb2 <bhr288103@gmail.com>
2024-12-20 09:47:04 +0300

    Update aljokerUB.py

--- JoKeRUB/core/aljokerUB.py
+++ JoKeRUB/core/aljokerUB.py
@@ -15,7 +15,7 @@ from telethon.errors.rpcerrorlist import AuthKeyDuplicatedError
 from telethon.sessions.string import _STRUCT_PREFORMAT, CURRENT_VERSION, StringSession
 
 
-LOGS = logging.getLogger("7rB  🤡")
+LOGS = logging.getLogger("robin")
 
 _PYRO_FORM = {351: ">B?256sI?", 356: ">B?256sQ?", 362: ">BI?256sQ?"}
 
