3a6c933bf1ec259afc09d8a79b729e8d5e69063b
uhrb2 <bhr288103@gmail.com>
2024-12-20 15:25:21 +0300

    Update المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -47,7 +47,7 @@ async def amireallyalive(event):
         cat_caption = f"مطورين سورس Robin \n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         cat_caption += f"- المطور  : @F_O_1\n"
-@U_9_O\n"
+ المطور  : @U_9_o\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         await event.client.send_file(
             event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
