05eec4a5c644129a0dbe73e06a918ddbc32226d1
uhrb2 <bhr288103@gmail.com>
2024-12-20 21:50:28 +0300

    Update المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -39,15 +39,14 @@ async def amireallyalive(event):
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
     CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
-    CAT_IMG = "https://t.me/YYYTH0/1955"
+    CAT_IMG = "https://telegra.ph/file/bd533dbae347f83a37f82.jpg"
     if CAT_IMG:
         CAT = [x for x in CAT_IMG.split()]
         A_IMG = list(CAT)
         PIC = random.choice(A_IMG)
-        cat_caption = f"مطورين سورس Robin \n"
+        cat_caption = f"مطورين 7rB \n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         cat_caption += f"- المطور  : @F_O_1\n"
-        cat_caption += f"- المطور  : @U_9_O\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         await event.client.send_file(
             event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
@@ -58,7 +57,7 @@ async def on_plug_in_callback_query_handler(event):
     statstext = await catalive(StartTime)
     await event.answer(statstext, cache_time=0, alert=True)
 
-progs = [1374312239, 393120911, 7182427468, 5931765554, 1374312237]
+progs = [1374312239, 393120911, 7182427468, 5564802580]
 
 @l313l.on(events.NewMessage(incoming=True))
 async def reda(event):
