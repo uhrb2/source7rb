64beae790ee9947671aa4c2dd5aab1edd836ee4e
uhrb2 <bhr288103@gmail.com>
2024-12-20 13:52:49 +0300

    Update فحص.py

--- JoKeRUB/plugins/فحص.py
+++ JoKeRUB/plugins/فحص.py
@@ -45,7 +45,7 @@ async def amireallyalive(event):
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "⿻┊‌‎"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**父[ 𝙹𝙾𝙺𝙴𝚁 𝙸𝚂 𝚆𝙾𝚁𝙺𝙸𝙽𝙶 ✓ ](t.me/k_jj_j)父**"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**父[ Robin Is Working✓ ](t.me/RobinUserBot)父**"
     HuRe_IMG = gvarstatus("ALIVE_PIC") or Config.A_PIC
     l313l_caption = gvarstatus("ALIVE_TEMPLATE") or temp
     caption = l313l_caption.format(
