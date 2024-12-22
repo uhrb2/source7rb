73b6975cc15db4359dc133a52c55d9f0ebfc5d16
uhrb2 <bhr288103@gmail.com>
2024-12-22 20:14:41 +0300

    Update المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -44,7 +44,7 @@ async def amireallyalive(event):
         CAT = [x for x in CAT_IMG.split()]
         A_IMG = list(CAT)
         PIC = random.choice(A_IMG)
-        cat_caption = f"مطورين 7rB \n"
+        cat_caption = f"مطورين سورس Robin \n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         cat_caption += f"- المطور  : @F_O_1\n"
         cat_caption += f"- المطور  : @U_9_O\n"
