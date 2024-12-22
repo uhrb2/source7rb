cd42a44f2556bec00363ec8ce7da1ba68d6e2a05
uhrb2 <bhr288103@gmail.com>
2024-12-22 20:59:30 +0300

    Update اوامري.py

--- JoKeRUB/plugins/اوامري.py
+++ JoKeRUB/plugins/اوامري.py
@@ -9,7 +9,7 @@ from JoKeRUB import l313l
 from ..core import check_owner
 from ..Config import Config
 
-
+JEP_IC = "https://telegra.ph/file/762989c65df81fc2e96d7.jpg"
 ROE = "**♰ هـذه هي قائمة اوامـر سـورس robin  ♰**"
 
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
