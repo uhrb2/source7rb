272f0f5d16b8ed13e41845b7c8e9b232847acd3a
uhrb2 <bhr288103@gmail.com>
2024-12-23 12:55:18 +0300

    Update المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -1,74 +1,20 @@
-import random
-import re
-import time
-from platform import python_version
+from TNT_HUNTER.__config import *
+from TNT_HUNTER.__xtelethon import *
+from telethon import events
 
-from telethon import version, Button, events
-from telethon.errors.rpcerrorlist import (
-    MediaEmptyError,
-    WebpageCurlFailedError,
-    WebpageMediaEmptyError,
-)
-from telethon.events import CallbackQuery
-
-from JoKeRUB import StartTime, l313l, JEPVERSION
-
-from ..Config import Config
-from ..core.managers import edit_or_reply
-from ..helpers.functions import catalive, check_data_base_heal_th, get_readable_time
-from ..helpers.utils import reply_id
-from ..sql_helper.globals import addgvar, delgvar, gvarstatus
-from . import mention
-
-plugin_category = "utils"
-
-@l313l.ar_cmd(
-    pattern="المطور$",
-    command=("المطور", plugin_category),
-    info={
-        "header": "لأظهار مطورين السورس",
-        "usage": [
-            "{tr}المطور",
-        ],
-    },
-)
-async def amireallyalive(event):
-    "A kind of showing bot details"
-    reply_to_id = await reply_id(event)
-    uptime = await get_readable_time((time.time() - StartTime))
-    _, check_sgnirts = check_data_base_heal_th()
-    EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
-    PIC = "https://t.me/t661h/5"
-    CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
-    cat_caption = f"مطورين سورس Robin \n"
-    cat_caption += f"✛━━━━━━━━━━━━━✛\n"
-    cat_caption += f"- المطور  : @F_O_1\n"
-    cat_caption += f"- المطور  : @U_9_O\n"
-    cat_caption += f"✛━━━━━━━━━━━━━✛\n"
-    await l313l.send_file(
-            event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
-        )
-
-@l313l.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
-async def on_plug_in_callback_query_handler(event):
-    statstext = await catalive(StartTime)
-    await event.answer(statstext, cache_time=0, alert=True)
-
-progs = [1374312239, 393120911, 7182427468, 5564802580]
-
-@l313l.on(events.NewMessage(incoming=True))
-async def reda(event):
-    if event.reply_to and event.sender_id in progs:
-       reply_msg = await event.get_reply_message()
-       owner_id = reply_msg.from_id.user_id
-       if owner_id == l313l.uid:
-           if event.message.message == "حظر من السورس":
-               await event.reply("**حاظر مطوري ، لقد تم حظره من استخدام السورس**")
-               addgvar("blockedfrom", "yes")
-           elif event.message.message == "الغاء الحظر من السورس":
-               await event.reply("**حاظر مطوري، لقد الغيت الحظر**")
-               delgvar("blockedfrom")
-                
-
-
+@TNT.on(events.NewMessage(outgoing=True, pattern=r".مطور"))
+async def _(event):
+    await event.edit("تم التجربة بنجاح")
+    await TNT.send_file(
+                event.chat_id,
+                file="https://t.me/floative/9",
+                caption=f"""
+Robin OWNER   : @F_O_1
+Robin Channel : @RobinUserBot
+Robin VERSION : `1.8`
+""",
+                link_preview=False,
+                force_document=False,
+                parse_mode=CustomParseMode('markdown'),
+            )
 
