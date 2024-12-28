b2bdf2b19c0c04c27494e6db8f912398ce5cb072
uhrb2 <bhr288103@gmail.com>
2024-12-28 14:03:28 +0300

    Update التقليد.py

--- JoKeRUB/plugins/التقليد.py
+++ JoKeRUB/plugins/التقليد.py
@@ -83,7 +83,7 @@ async def echo(event):
 
 
 @l313l.ar_cmd(
-    pattern=admin_cmd(pattern="،،الغاء التقليد")
+    @l313l.on(admin_cmd(pattern="،،الغاء التقليد"))
     command=("الغاء التقليد", plugin_category),
     info={
         "header": "To delete echo in this chat.",
