10448664e486bd72a73436aae0d7c98aa7682574
uhrb2 <bhr288103@gmail.com>
2025-01-22 09:38:12 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -6,7 +6,7 @@ class Development(Config):
     # the name to display in your alive message
     ALIVE_NAME = "كارمن"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgresql://joker210:joker210@localhost:5432/joker210"
+    DB_URI = "postgresql://joker211:joker211@localhost:5432/joker211"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
     STRING_SESSION = "1BJWap1wBu1IdoJSDvL4YEvOiz49u1d0jBW_CAfyZmLYN4ce1AtSfdGpC9FUK0aEgeU55z1eJ_mRLh-dz0laPvzfpsG6t7vkc4iysdz7rYma1M0wrzSzmo4z-CvVmoR6SPkS7l_SaF3ODzlXsQKM_CMXmrUjZ6XZaP5GnSBHL-qm8F5RgrhipRPgod08ZwdGINZ-ZtiKGaqmTlo8RMh5MYxcJpZSoxBPQCEFOtzkX-5m68TJIVOjZftThVYQSsC-j74vdn3UraWgHbKeo161BChDPahjSxZeFIiQrAcwsfgThv1VwgJZCwrCjMMa-2PvvVAIXX5iWdUxQlJftnAGU1P6l9ZUf_3s="
     # create a new bot in @botfather and fill the following vales with bottoken and username respectively
