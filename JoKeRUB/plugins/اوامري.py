b773abddbc1d4158908aa88b6dcdb8debd0d74fd
uhrb2 <bhr288103@gmail.com>
2025-01-24 16:20:53 +0300

    Update اوامري.py

--- JoKeRUB/plugins/اوامري.py
+++ JoKeRUB/plugins/اوامري.py
@@ -24,7 +24,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
             buttons = [
                 [Button.inline("☪︎ اوامر الادمن ☪︎", data="l313l0")],
                 [
-                    Button.inline("☪︎ اوامر البوت ☪︎", data="rozbot"),
+                    Button.inline(" اوامر البوت ☪︎", data="rozbot"),
                     Button.inline("☪︎ الحساب ☪︎", data="Jmrz"),
                     Button.inline("☪︎ المجموعات ☪︎", data="gro"),
                 ],
