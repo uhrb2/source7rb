52c0be8c6e05ab5a8c0e672d5c6edff002f186dd
uhrb2 <bhr288103@gmail.com>
2025-02-03 11:35:54 +0200

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -2,19 +2,18 @@ from sample_config import Config
 class Development(Config):
     # get this values from the my.telegram.org
     APP_ID = "24347380"
-    API_HASH = "1ad5dea4dfdddfed44df611dcd0d1736"
+        API_HASH = "1ad5dea4dfdddfed44df611dcd0d1736"
     # the name to display in your alive message
-    ALIVE_NAME = ."
+    ALIVE_NAME = "."
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgresql://jokeer29:jokeer29@localhost:5432/jokeer29"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
     STRING_SESSION = "1BJWap1sBu5rExWRFLdq9xQ4MxT6PxDyqwy_SkWp32BSZWbjBtk0AWNnQw3cvdhcfAo-b14hNt9-FQEzoEUXzYKcyk6JAFUWXA8KjY_09ulrQLNn-GttslRwFgCX3R63JytvAaxj8wNFqaNKWzEZJswfslUsQam5bkAWk52XIa8UzZMmn7Y58GnsKWHMkk_mZxrMiAeloqZkbKUTUc-t2WoVfJgSpxKJj6fRbpal8-vFuj0vUGcmD-6m8voUbU942rVtOwBiAei2xz0HR1brXxA2HJxcdwzJPDTmSIex9mHQnKL84GcSI0mGlsmuH-su7owEepwkTpAp7WnHu9NHtsoN49jiQOpE="
     # create a new bot in @botfather and fill the following vales with bottoken and username respectively
     TG_BOT_TOKEN = "8054591974:AAFhKZsSy89YletWX72kKxQ9vxTZs-FBLL4"
-    # command
+    # command handler
     COMMAND_HAND_LER = "."
     # sudo enter the id of sudo users userid's in that array
     SUDO_USERS = []
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
-
