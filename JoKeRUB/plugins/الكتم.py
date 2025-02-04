bd2be5f19f3b427ef2b601eccbbc0770f74e26e0
uhrb2 <bhr288103@gmail.com>
2025-02-04 08:56:26 +0200

    Update الكتم.py

--- JoKeRUB/plugins/الكتم.py
+++ JoKeRUB/plugins/الكتم.py
@@ -18,8 +18,8 @@ from . import BOTLOG, BOTLOG_CHATID, admin_groups, get_user_from_event
 
 plugin_category = "admin"
 joker_users = []
-joker_mute = "https://telegra.ph/file/c5ef9550465a47845c626.jpg"
-joker_unmute = "https://telegra.ph/file/e9473ddef0b58cdd7f9e7.jpg"
+joker_mute = "telegra.ph/file/f3b760e4a99340d331f9b.jpg"
+joker_unmute = "telegra.ph/file/f3b760e4a99340d331f9b.jpg"
 import os
 
 file_path = '7rB Mute.txt'
