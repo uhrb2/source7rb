e206ac36c10ded9ec7d34b3f1bfb1464e7698ac9
uhrb2 <bhr288103@gmail.com>
2025-02-06 12:23:22 +0200

    Update همسه.py

--- JoKeRUB/plugins/همسه.py
+++ JoKeRUB/plugins/همسه.py
@@ -46,3 +46,46 @@ async def gamez(event):
     tap = await bot.inline_query(jmusername, uunzz)
     await tap[0].click(event.chat_id)
     await event.delete()
+
+
+@borg.on(
+    admin_cmd(pattern="ميمز ?(.*)")
+)
+async def wspr(event):
+    if event.fwd_from:
+        return
+    l313lb = event.pattern_match.group(1)
+    rrrd7 = "@iizbot"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(rrrd7, l313lb) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+@borg.on(admin_cmd("م27"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("اوامر الهمسه واكس او \n\n⌔︙الامر  • `.همسة`\n⌔︙الاستخدام  • لكتابة همسه سرية لشخص في المجموعه \n\n᯽︙ الامر • `.الهمسة`\n᯽︙ استخدامه • لعرض كيفية كتابة همسة سرية\n\n᯽︙ الامر • `.اكس او `\n ᯽︙ استخدامه • ففط ارسل الامر لبدء لعبة اكس او\n\n᯽︙ CH  - @RobinUserBot")
+
+@borg.on(admin_cmd("الهمسة"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("**᯽︙ شـرح كيـفية كـتابة همـسة سـرية**\n اولا اكتب الامر  .همسة  بعدها الرسالة بعدها اكتب معرف الشخص\n مـثال  :   `.همسة ههلا @F_O_1`")
+
+@borg.on(
+    admin_cmd(
+       pattern="اكس او$"
+    )
+)
+# كتابة وتعديل فريق 7rB   #@F_O_1
+#تم التعديل مره أخرى بواسطة برلين 
+async def gamez(event):
+    if event.fwd_from:
+        return
+    jmusername = "@xoBot"
+    uunzz = "play"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(jmusername, uunzz)
+    await tap[0].click(event.chat_id)
+    await event.delete()
