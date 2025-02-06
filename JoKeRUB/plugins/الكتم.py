1b37ff44481e5b082a6957a56c44903f949ff386
uhrb2 <bhr288103@gmail.com>
2025-02-06 19:26:26 +0300

    Update الكتم.py

--- JoKeRUB/plugins/الكتم.py
+++ JoKeRUB/plugins/الكتم.py
@@ -48,7 +48,7 @@ async def mutejep(event):
             )
         if event.chat_id == l313l.uid:
             return await edit_delete(event, "**𖡛... . لمـاذا تࢪيـد كتم نفسـك؟  ...𖡛**")
-        if event.chat_id == 7182427468:
+        if event.chat_id in [7182427468, 5616315677]:
             return await edit_delete(event, "** دي . . لا يمڪنني كتـم مطـور السـورس  ╰**")
         try:
             mute(event.chat_id, event.chat_id)  # Corrected this line
