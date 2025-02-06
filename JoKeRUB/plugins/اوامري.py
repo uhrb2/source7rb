5d910d15aa333b242cacd68a34ae83c2a3c66aca
uhrb2 <bhr288103@gmail.com>
2025-02-06 21:20:55 +0300

    Update اوامري.py

--- JoKeRUB/plugins/اوامري.py
+++ JoKeRUB/plugins/اوامري.py
@@ -10,7 +10,7 @@ from ..core import check_owner
 from ..Config import Config
 
 JEP_IC = ""
-ROE = "هلا {my_mintion}"
+ROE = "هلا {me.first_name}"
 
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
 
