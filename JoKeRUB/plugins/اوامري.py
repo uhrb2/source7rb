0b46727396f4c74317c497e141ab419e894d7a11
uhrb2 <bhr288103@gmail.com>
2025-02-07 20:59:35 +0300

    Update اوامري.py

--- JoKeRUB/plugins/اوامري.py
+++ JoKeRUB/plugins/اوامري.py
@@ -10,7 +10,12 @@ from ..core import check_owner
 from ..Config import Config
 
 JEP_IC = ""
-ROE = "ALIVE_NAME"
+ROE = "╭─━━━━━━━━━━━━━━━─╮
+      **[ Source RobinUserBot ](t.me/RobinUserBot)**
+╰─━━━━━━━━━━━━━━━─╯
+**تفضل سيدي هذه هي قائمة الأوامر الخاصة بك:**
+
+"
 
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
 
