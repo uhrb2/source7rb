80ccefafaad4ba867cad8e2266a2651187ef38b1
uhrb2 <bhr288103@gmail.com>
2025-02-08 10:12:30 +0300

    Update اوامري.py

--- JoKeRUB/plugins/اوامري.py
+++ JoKeRUB/plugins/اوامري.py
@@ -49,8 +49,9 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
                 ],
                 [
                     Button.inline("📭 الأكستـرا ", data="iiers"),
-                    Button.inline("🎟️ الانتحال والتقليد ", data="uscuxrz"),
+                    Button.inline("🪇 الانتحال والتقليد ", data="uscuxrz"),
                 ],
+            ]
             if JEP_IC and JEP_IC.endswith((".jpg", ".png", "gif", "mp4")):
                 result = builder.photo(
                     JEP_IC, text=ROE, buttons=buttons, link_preview=False
