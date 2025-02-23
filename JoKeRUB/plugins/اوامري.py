6c443b2238c2e9947c8c41f228e6ce62be6b2483
uhrb2 <bhr288103@gmail.com>
2025-02-23 14:53:19 +0300

    Update اوامري.py

--- JoKeRUB/plugins/اوامري.py
+++ JoKeRUB/plugins/اوامري.py
@@ -9,7 +9,7 @@ from JoKeRUB import l313l
 from ..core import check_owner
 from ..Config import Config
 
-JEP_IC = "https://t.me/Imain3/1985"
+JEP_IC = "https://t.me/Imain3/1993"
 ROE = f"**☆┊لـَوحـة أوامـِر RobinUserBot الشفـافَـة**\n**☆┊المستخـِدم ↶** {mention} \n\n "
 
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
