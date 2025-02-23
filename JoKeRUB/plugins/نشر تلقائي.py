d871e3192dc4b605b0dde9730dcd0869f915b542
uhrb2 <bhr288103@gmail.com>
2025-02-23 14:50:31 +0300

    Update نشر تلقائي.py

--- JoKeRUB/plugins/نشر تلقائي.py
+++ JoKeRUB/plugins/نشر تلقائي.py
@@ -24,8 +24,8 @@ async def get_user_from_event(event):
         if user.isnumeric():
             user = int(user)
         if not user:
-            self_user = await event.client.get_me()
-            user = self_user.id
+            user_object = await event.client.get_me()
+            user = user_object.id
         if event.message.entities:
             probable_user_mention_entity = event.message.entities[0]
             if isinstance(probable_user_mention_entity, MessageEntityMentionName):
@@ -44,7 +44,7 @@ async def get_user_from_event(event):
 
 @l313l.on(admin_cmd(pattern="(نشر_تلقائي|النشر_التلقائي)"))
 async def _(event):
-    if (event.is_private or event.is_group):
+    if event.is_private or event.is_group:
         return await edit_or_reply(event, "**᯽︙ عـذراً .. النشر التلقائي خـاص بالقنـوات فقـط**")
     jok = event.pattern_match.group(1)
     if not jok:
@@ -63,17 +63,16 @@ async def _(event):
     try:
         JoKeRUB = (await event.client.get_entity(JoKeRUB)).id
     except BaseException:
-        return await event.reply("**᯽︙ عـذراً .. معـرف/ايـدي القنـاة غيـر صـالح**\n**✾╎الرجـاء التـأكـد مـن المعـرف/الايـدي**")
-    if is_post(str(JoKeRUB) , event.chat_id):
-        return await edit_or_reply(event, "**᯽︙ النشـر التلقـائي من القنـاة ** `{jok}` **مفعـل مسبقـاً ✓**")
+        return await edit_or_reply(event, "**᯽︙ عـذراً .. معـرف/ايـدي القنـاة غيـر صـالح**\n**✾╎الرجـاء التـأكـد مـن المعـرف/الايـدي**")
+    if is_post(str(JoKeRUB), event.chat_id):
+        return await edit_or_reply(event, f"**᯽︙ النشـر التلقـائي من القنـاة ** `{jok}` **مفعـل مسبقـاً ✓**")
     add_post(str(JoKeRUB), event.chat_id)
     await edit_or_reply(event, f"**᯽︙ تم تفعيـل النشـر التلقـائي من القنـاة ** `{jok}` **بنجـاح ✓**")
 
 
-
 @l313l.on(admin_cmd(pattern="(ايقاف_نشر|ايقاف_النشر)"))
 async def _(event):
-    if (event.is_private or event.is_group):
+    if event.is_private or event.is_group:
         return await edit_or_reply(event, "**᯽︙ عـذراً .. النشر التلقائي خـاص بالقنـوات فقـط**")
     jok = event.pattern_match.group(1)
     if not jok:
@@ -92,7 +91,7 @@ async def _(event):
     try:
         JoKeRUB = (await event.client.get_entity(JoKeRUB)).id
     except BaseException:
-        return await event.reply("**᯽︙ عـذراً .. معـرف/ايـدي القنـاة غيـر صـالح**\n**✾╎الرجـاء التـأكـد مـن المعـرف/الايـدي**")
+        return await edit_or_reply(event, "**᯽︙ عـذراً .. معـرف/ايـدي القنـاة غيـر صـالح**\n**✾╎الرجـاء التـأكـد مـن المعـرف/الايـدي**")
     if not is_post(str(JoKeRUB), event.chat_id):
         return await edit_or_reply(event, "**᯽︙ تم تعطيـل النشر التلقـائي لهـذه القنـاة هنـا .. بنجـاح ✓**")
     remove_post(str(JoKeRUB), event.chat_id)
