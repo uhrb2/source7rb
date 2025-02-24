514411e133a41b06550404e6dc65a4d7c9877926
uhrb2 <bhr288103@gmail.com>
2025-02-24 20:38:13 +0300

    Update الاتصال.py

--- JoKeRUB/plugins/الاتصال.py
+++ JoKeRUB/plugins/الاتصال.py
@@ -18,30 +18,26 @@ from ..helpers.utils import reply_id, _catutils, parse_pre, yaml_format, install
 
 plugin_category = "utils"
 
-# قائمة لحفظ رتب المستخدمين
-user_ranks = {}
-
 @l313l.on(admin_cmd(pattern="رفع(?: |$)([\s\S]*)"))
 async def promote_user(event):
     match = event.pattern_match.group(1).strip()
     if not match:
-        return await event.respond("**- يرجى تحديد الدور المطلوب**")
+        return await edit_or_reply(event, "**- يرجى تحديد الدور المطلوب**")
     
     user, custom = await get_user_from_event(event)
     if not user:
-        return await event.respond("**- لـم استطـع العثــور ع الشخــص**")
+        return await edit_or_reply(event, "**- لـم استطـع العثــور ع الشخــص**")
 
     user_id = user.id
     user_name = user.first_name.replace("\u2060", "") if user.first_name else user.username
 
     # تحقق من عدم رفع المطور
-    if user_id in [7182427468]:
-        return await event.respond(f"**- لكك دي هذا المطور**")
+    if user_id == 7182427468 or user_id == 393120911:
+        return await edit_or_reply(event, f"**- لكك دي هذا المطور**")
     
     me = await event.client.get_me()
     my_mention = f"[{me.first_name}](tg://user?id={me.id})"
 
-    # تحديث رتبة المستخدم
-    user_ranks[{rotbat}] = match
+    await edit_or_reply(event, f"**᯽︙ المستخدم** [{user_name}](tg://user?id={user.id}) \n**᯽︙  تـم رفعـه {match} بواسطة :** {my_mention}")
 
-    await event.respond(f"**᯽︙ المستخدم** [{user_name}](tg://user?id={user.id}) \n**᯽︙  تـم رفعـه {match} بواسطة :** {my_mention}")
+# بقية الأكواد...
