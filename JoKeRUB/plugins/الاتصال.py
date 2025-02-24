05afed6d3fd5d34fa79ffb7710310be61c88f4cd
uhrb2 <bhr288103@gmail.com>
2025-02-24 19:21:00 +0300

    Update الاتصال.py

--- JoKeRUB/plugins/الاتصال.py
+++ JoKeRUB/plugins/الاتصال.py
@@ -1,56 +1,65 @@
 import html
 import os
+import random
 from requests import get
 from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.utils import get_input_location
-from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from JoKeRUB import l313l
-from JoKeRUB.core.logger import logging
+from random import choice
+from l313l.razan.resources.strings import *
+from telethon import events
 from ..Config import Config
 from ..core.managers import edit_or_reply
 from ..helpers import get_user_from_event, reply_id
 from . import spamwatch
+from telethon.utils import get_display_name
+from ..helpers.utils import reply_id, _catutils, parse_pre, yaml_format, install_pip, get_user_from_event, _format
 
+plugin_category = "utils"
 
-@l313l.on(admin_cmd(pattern="رفع مساعد(?: |$)(.*)",
-    command=("رفع مساعد", plugin_category),
-    info={
-        "header": "لـ رفع مستخدم كـ مطور مساعد",
-        "الاستـخـدام": " {tr}رفع مساعد بالـرد او {tr}رفع مساعد + معـرف/ايـدي الشخص",
-    },
-)
-async def promote_assistant(event):
-    "Promotes a user to assistant developer"
-    replied_user = await get_user_from_event(event)
-    if not replied_user:
+@l313l.on(admin_cmd(pattern="رفع(?: |$)([\s\S]*)"))
+async def promote_user(event):
+    match = event.pattern_match.group(1).strip()
+    if not match:
+        return await edit_or_reply(event, "**- يرجى تحديد الدور المطلوب**")
+    
+    roles = {
+        "مساعد": "مطور مساعد",
+        "ادمن": "ادمن",
+        "مميز": "مميز",
+        "مرت": "مرتك",
+        "جلب": "جلب",
+        "تاج": "تاج",
+        "قرد": "قرد",
+        "بكلبي": "بكلبك",
+        "مطي": "مطي",
+        "زوجي": "زوجج",
+        "زاحف": "زاحف",
+        "كحبة": "كحبة",
+        "فرخ": "فرخ",
+        "حاتة": "حاتة",
+        "هايشة": "هايشة",
+        "صاك": "صاك",
+        "ايجة": "ايچة",
+        "زبال": "زبال",
+        "كواد": "كواد",
+        "ديوث": "ديوث"
+    }
+
+    role = roles.get(match)
+    if not role:
+        return await edit_or_reply(event, "**- الدور غير صالح. يرجى تحديد دور صحيح**")
+
+    user, custom = await get_user_from_event(event)
+    if not user:
         return await edit_or_reply(event, "**- لـم استطـع العثــور ع الشخــص**")
-    user_id = replied_user.id
-    assistant_developers = gvarstatus("assistant_developers") or ""
-    if str(user_id) in assistant_developers.split():
-        return await edit_or_reply(event, "**- المستخدم هـو مطور مساعد بالفعل**")
-    assistant_developers += f" {user_id}"
-    addgvar("assistant_developers", assistant_developers.strip())
-    await edit_or_reply(event, f"**- تـم رفـع {replied_user.first_name} كـ مطور مساعد بنجاح**")
 
-@l313l.on(admin_cmd(pattern="تنزيل مساعد(?: |$)(.*)",
-    command=("تنزيل مساعد", plugin_category),
-    info={
-        "header": "لـ تنزيل مستخدم من مطور مساعد",
-        "الاستـخـدام": " {tr}تنزيل مساعد بالـرد او {tr}تنزيل مساعد + معـرف/ايـدي الشخص",
-    },
-)
-async def demote_assistant(event):
-    "Demotes a user from assistant developer"
-    replied_user = await get_user_from_event(event)
-    if not replied_user:
-        return await edit_or_reply(event, "**- لـم استطـع العثــور ع الشخــص**")
-    user_id = replied_user.id
-    assistant_developers = gvarstatus("assistant_developers") or ""
-    if str(user_id) not in assistant_developers.split():
-        return await edit_or_reply(event, "**- المستخدم لـيس مطور مساعد**")
-    updated_assistant_developers = " ".join([uid for uid in assistant_developers.split() if uid != str(user_id)])
-    addgvar("assistant_developers", updated_assistant_developers.strip())
-    await edit_or_reply(event, f"**- تـم تنزيل {replied_user.first_name} من مطور مساعد بنجاح**")
+    user_id = user.id
+    user_name = user.first_name.replace("\u2060", "") if user.first_name else user.username
+    me = await event.client.get_me()
+    my_mention = f"[{me.first_name}](tg://user?id={me.id})"
+
+    await edit_or_reply(event, f"**᯽︙ المستخدم** [{user_name}](tg://user?id={user.id}) \n**᯽︙  تـم رفعـه {role} بواسطة :** {my_mention}")
 
 # بقية الأكواد...
