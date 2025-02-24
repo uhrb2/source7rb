fe37a2ca492d4344d9ff94bb7b1834cbcf64dc65
uhrb2 <bhr288103@gmail.com>
2025-02-24 09:52:58 +0300

    Update ذاتية.py

--- JoKeRUB/plugins/ذاتية.py
+++ JoKeRUB/plugins/ذاتية.py
@@ -1,4 +1,4 @@
-from telethon import events
+٥from telethon import events
 import random, re
 from JoKeRUB.utils import admin_cmd
 import asyncio 
@@ -325,7 +325,7 @@ from JoKeRUB import *
 
 # باقي الكود...
 
-@l313l.on(admin_cmd(pattern="كشف انشاء الحساب ?(.*)"))
+@l313l.on(admin_cmd(pattern="معرفه انشاء الحساب ?(.*)"))
 async def account_creation_date(event):
     """معرفة تاريخ إنشاء الحساب"""
     user = await event.get_reply_message()
