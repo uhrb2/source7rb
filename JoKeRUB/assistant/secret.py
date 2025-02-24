0b5f48307e5cef9853062f27013ef0bb7793ffe0
uhrb2 <bhr288103@gmail.com>
2025-02-24 17:30:43 +0300

    Update secret.py

--- JoKeRUB/assistant/secret.py
+++ JoKeRUB/assistant/secret.py
@@ -3,27 +3,25 @@ import os
 import re
 
 from telethon.events import CallbackQuery
-from telethon.tl.functions.users import GetUsersRequest
 
-from zira import zedub
-from ..sql_helper.globals import gvarstatus
+from JoKeRUB import l313l
 
 
-@zedub.tgbot.on(CallbackQuery(data=re.compile(b"secret_(.*)")))
+@l313l.tgbot.on(CallbackQuery(data=re.compile(b"secret_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
     uzerid = gvarstatus("hmsa_id")
     ussr = int(uzerid) if uzerid.isdigit() else uzerid
     try:
-        zzz = await zedub.get_entity(ussr)
+        zzz = await l313l.get_entity(ussr)
     except ValueError:
-        zzz = await zedub(GetUsersRequest(ussr))
+        zzz = await l313l(GetUsersRequest(ussr))
     if os.path.exists("./zira/secret.txt"):
         jsondata = json.load(open("./zira/secret.txt"))
         try:
             message = jsondata[f"{timestamp}"]
             userid = message["userid"]
-            ids = [userid, zedub.uid, zzz.id]
+            ids = [userid, l313l.uid, zzz.id]
             if event.query.user_id in ids:
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
