5b7ef457df56aac15f3cfc837cea65a25cd5bd00
uhrb2 <bhr288103@gmail.com>
2025-02-25 14:14:31 +0300

    Update صياد.py

--- JoKeRUB/plugins/صياد.py
+++ JoKeRUB/plugins/صياد.py
@@ -11,7 +11,13 @@ from JoKeRUB import l313l
 from ..core.managers import edit_or_reply
 
 trys = [0]
+crys = [0]
+arys = [0]
+brys = [0]
 itsclim = ["off"]
+iscuto = ["off"]
+istuto = ["off"]
+isbuto = ["off"]
 
 async def check_user(username):
     url = "https://t.me/" + str(username)
@@ -33,99 +39,343 @@ async def check_user(username):
     else:
         return False
 
-async def gen_user():
+async def checker_user(username):
+    url = "https://t.me/" + str(username)
+    headers = {
+        "User-Agent": generate_user_agent(),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7",
+    }
+
+    response = requests.get(url, headers=headers)
+    if (
+        response.text.find(
+            'If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"'
+        )
+        >= 0
+    ):
+        return True
+    else:
+        return False
+
+async def gen_user(choice):
     a = "qwertyuiopasdfghjklzxcvbnm"
     b = "1234567890"
     e = "qwertyuiopasdfghjklzxcvbnm1234567890"
-    username = "".join(random.choices(a + b, k=6))
+    z = "sdfghjklzwerty1234567890uioxcvbqpanm"
+    o = "0987654321"
+    q = "5432109876"
+    k = "mnbvcxzlkjhgfdsapoiuytrewq"
+    if choice == "سداسي_حرفين1": #ARAAAR
+        c = d = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], d[0], c[0], c[0], c[0], d[0]]
+        random.shuffle(f)
+        username = "".join(f)
+
+    elif choice == "سداسي_شرطه": #AAAA_R ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], c[0], c[0], "_", d[0]]
+        username = "".join(f)
+
+    elif choice == "سداسي_حرفين2": #AAAARR ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], c[0], c[0], d[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "سداسي_حرفين3": #AAARRA ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], c[0], d[0], d[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سداسي_حرفين4": #AARRAA ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], d[0], d[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سداسي_حرفين5": #ARRAAA ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], d[0], d[0], c[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سداسي_حرفين6": #AARRRR ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], d[0], d[0], d[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "ثلاثي1": #A_R_D
+        c = random.choices(a)
+        d = random.choices(e)
+        s = random.choices(z)
+        f = [c[0], "_", d[0], "_", s[0]]
+        username = "".join(f)
+
+    elif choice == "ثلاثي2": #A_7_R ~ 
+        c = random.choices(a)
+        d = random.choices(o)
+        s = random.choices(z)
+        f = [c[0], "_", d[0], "_", s[0]]
+        username = "".join(f)
+
+    elif choice == "ثلاثي3": #A_7_0 ~ 
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(o)
+        f = [c[0], "_", d[0], "_", s[0]]
+        username = "".join(f)
+
+    elif choice == "شبه رباعي1": #A_A_A_R ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", c[0], "_", c[0], "_", d[0]]
+        username = "".join(f)
+
+    elif choice == "شبه رباعي2": #A_R_R_R ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", d[0], "_", d[0], "_", d[0]]
+        username = "".join(f)
+
+    elif choice == "شبه رباعي3": #A_RR_A ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", d[0], d[0], "_", c[0]]
+        username = "".join(f)
+
+    elif choice == "شبه رباعي4": #A_RR_R ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", d[0], d[0], "_", d[0]]
+        username = "".join(f)
+    elif choice == "رباعي1": #AAA_R ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], c[0], "_", d[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي2": #A_RRR ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", d[0], d[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي3": #AA_RR ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], "_", d[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي4": #AA_AR ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], "_", c[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي5": #AA_RA ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], "_", d[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي6": #AR_RA ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], d[0], "_", d[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي7": #AR_AR ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], d[0], "_", c[0], d[0]]
+        username = "".join(f)
+
+
+    elif choice == "رباعي8": #AR_RR ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], d[0], "_", d[0], d[0]]
+        username = "".join(f)
+
+
+    elif choice == "بوتات1": #AR_Bot ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], d[0], "_", "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات2": #A_RBot ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", d[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات3": #AR7Bot ~ 
+        c = random.choices(a)
+        d = random.choices(k)
+        s = random.choices(b)
+        f = [c[0], d[0], s[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات4": #A7RBot ~ 
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(k)
+        f = [c[0], d[0], s[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات5": #A77Bot ~ 
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(o)
+        f = [c[0], d[0], s[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات6": #ADRBot
+        c = random.choices(a)
+        d = random.choices(e)
+        s = random.choices(z)
+        f = [c[0], d[0], s[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات7": #(AARBot - AA8bot) ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], d[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات8": #AARBot ~ 
+        c = random.choices(a)
+        d = random.choices(k)
+        f = [c[0], c[0], d[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات9": #AA8Bot ~ 
+        c = random.choices(a)
+        d = random.choices(o)
+        f = [c[0], c[0], d[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "خماسي حرفين1": #AAARD ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        s = random.choices(e)
+        f = [c[0], c[0], c[0], s[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "خماسي ارقام": #AR888 ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        s = random.choices(b)
+        f = [c[0], d[0], s[0], s[0], s[0]]
+        username = "".join(f)
+
+    elif choice == "خماسي حرفين2": #A7RRR ~ 
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(z)
+        f = [c[0], d[0], s[0], s[0], s[0]]
+        username = "".join(f)
+
+
+
+
+    elif choice == "خماسي حرفين3": #ARRRD ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        s = random.choices(e)
+        f = [c[0], d[0], d[0], d[0], s[0]]
+        username = "".join(f)
+
+
+    elif choice == "سباعيات1": #AAAAAAR ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], c[0], c[0], c[0], c[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات2": #AAAAARA ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], c[0], c[0], c[0], d[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات3": #AAAARAA
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], c[0], c[0], d[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات4": #AAARAAA ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], c[0], d[0], c[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات5": #AARAAAA ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], d[0], c[0], c[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات6": #ARAAAAA ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], d[0], c[0], c[0], c[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات7": #ARRRRRR ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], d[0], d[0], d[0], d[0], d[0], d[0]]
+        username = "".join(f)
+    elif choice == "متاح":
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], d[0], d[0], d[0], d[0], d[0], d[0], c[0], d[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "ايقاف": #
+        return "stop"
+    else:
+        return "error"
     return username
 
-@l313l.ar_cmd(pattern="صيد متاح")
-async def available_hunt(event):
-    try:
-        rub = f"@{l313l.me.username}" if l313l.me.username else ""
-        ch = await l313l(
-            functions.channels.CreateChannelRequest(
-                title="صيد متاح",
-                about=f"This channel to hunt username by - @AvailableHuntBot | {rub}",
-            )
-        )
-        try:
-            ch = ch.updates[1].channel_id
-        except Exception:
-            ch = ch.chats[0].id
-        
-        await edit_or_reply(
-            event,
-            f"**✅│تم بدء عملية الصيد بنجاح!**\n\n"
-            "**📊│لمتابعة حالة عملية الصيد، أرسل:** `.حالة الصيد`\n"
-            "**⛔️│لإيقاف عملية الصيد، أرسل:** `.صيد ايقاف`"
-        )
-    except Exception as e:
-        await l313l.send_message(
-            event.chat_id,
-            f"**⛔️│عذرًا، حدث خطأ أثناء إنشاء القناة.**\n\n"
-            f"🔹 **تفاصيل الخطأ:**\n`{str(e)}`"
-        )
-        vedmod = False
 
-    itsclim.clear()
-    itsclim.append("on")
-    vedmod = True
-    while vedmod:
-        username = await gen_user()
-        if username == "stop":
-            itsclim.clear()
-            itsclim.append("off")
-            trys[0] = 0
-            await edit_or_reply(
-                event,
-                "**✅│تم إيقاف عملية الصيد بنجاح!**"
-            )
-            break
-        isav = await check_user(username)
-        if isav == True:
-            try:
-                await l313l(
-                    functions.channels.UpdateUsernameRequest(
-                        channel=ch, username=username
-                    )
-                )
-                await event.client.send_message(event.chat_id,
-                                                f"**✅│تم الصيد بنجاح!**\n\n"
-                                                f"🔹 **المعرف:** @{username}\n"
-                                                f"🔹 **بواسطة:** @AvailableHuntBot\n"
-                                                f"🔹 **عدد المحاولات:** {trys[0]}"
-                                                )
-                vedmod = False
-                break
-            except FloodWaitError as hrb:
-                wait_time = hrb.seconds
-                await sleep(wait_time + 10)
-                pass
-            except telethon.errors.rpcerrorlist.UsernameInvalidError:
-                pass
-            except telethon.errors.FloodError as e:
-                flood_error = e.seconds
-                await sleep(flood_error + 10)
-                pass
-            except Exception as e:
-                if "too many public channels" in str(e):
-                    await l313l.send_message(event.chat_id,
-                                                f"""**⛔️│خطأ أثناء محاولة صيد المعرف:** @{username}\n\n"""
-                                                "**🔹│سبب الخطأ:**\n"
-                                                "لقد تجاوزت الحد المسموح به لإنشاء القنوات العامة.\n"
-                                                "**🔧│الحل:** قم بحذف قناة عامة واحدة أو أكثر من قنواتك الحالية لتتمكن من صيد هذا المعرف."
-                                            )
-                    break
-                else:
-                    pass
-        else:
-            pass
-        trys[0] += 1
-        await asyncio.sleep(1)
-
-    itsclim.clear()
-    itsclim.append("off")
-    trys[0] = 0
-    return await event.client.send_message(event.chat_id, "**✅│تم الانتهاء من عملية الصيد بنجاح!**")
+hrrrbChecler_cmd = (
+    "🔺 اوامـر الصيـد والتثبيت 🔻\n\n"
+    "🟢╎ أولاً: قائمة أوامر تشيكـر صيد معرفات تيليجرام:\n"
+    "- **`.النوع`**\n"
+    "  ⪼ لعرض الأنواع التي يمكن صيدها مع الأمثلة.\n"
+    "- **`.صيد` + النوع**\n"
+    "  ⪼ لصيد يوزرات عشوائية حسب النوع المحدد.\n"
+    "- **`.حالة الصيد`**\n"
+    "  ⪼ لمعرفة حالة تقدم عملية الصيد.\n"
+    "- **`.صيد ايقاف`**\n"
+    "  ⪼ لإيقاف عملية الصيد الحالية.\n\n"
+    "🔴╎ ثانياً: قائمة أوامر تشيكـر تثبيت معرفات تيليجرام:\n"
+    "- **`.تثبيت_قناة` + اليوزر**\n"
+    "  ⪼ لتثبيت اليوزر بقناة معينة إذا أصبح متاحًا.\n"
+    "- **`.تثبيت_حساب` + اليوزر**\n"
+    "  ⪼ لتثبيت اليوزر بحسابك مباشرة إذا أصبح متاحًا.\n"
+    "- **`.تثبيت_بوت` + اليوزر**\n"
+    "  ⪼ لتثبيت اليوزر في بوت فاذر إذا أصبح متاحًا.\n\n"
+    "🟡╎ اوامر حالة التثبيت :\n"
+    "- **`.حالة تثبيت_القناة`**\n"
+    "  ⪼ لمعرفة حالة تقدم التثبيت التلقائي على القناة.\n"
+    "- **`.حالة تثبيت_الحساب`**\n"
+    "  ⪼ لمعرفة حالة تقدم التثبيت التلقائي على حسابك.\n"
+    "- **`.حالة تثبيت_البوت`**\n"
+    "  ⪼ لمعرفة حالة تقدم التثبيت التلقائي على بوت فاذر.\n\n"
+    "🟠╎ اوامر أيقاف التثبيت :\n"
+    "- **`.ايقاف تثبيت_القناة`**\n"
+    "  ⪼ لإيقاف عملية تثبيت القناة التلقائي.\n"
+    "- **`.ايقاف تثبيت_الحساب`**\n"
+    "  ⪼ لإيقاف عملية تثبيت الحساب التلقائي.\n"
+    "- **`.
