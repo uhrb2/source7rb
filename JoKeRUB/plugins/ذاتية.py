5eba9d95b926da865d1834865ded0055429d70b3
uhrb2 <bhr288103@gmail.com>
2025-02-26 21:14:53 +0300

    Update ذاتية.py

--- JoKeRUB/plugins/ذاتية.py
+++ JoKeRUB/plugins/ذاتية.py
@@ -122,46 +122,6 @@ async def gamez(event):
     await tap[0].click(event.chat_id)
     await event.delete()
 
-@l313l.on(admin_cmd(pattern="(الصوتية تشغيل|صوتية تشغيل)"))
-async def enable_voice_save(event):
-    if gvarstatus("savevoiceforme"):
-        return await edit_delete(event, "**᯽︙حفظ الصوتيات مفعل وليس بحاجة للتفعيل مجدداً **")
-    else:
-        addgvar("savevoiceforme", "enabled")
-        await edit_delete(event, "**᯽︙تم تفعيل ميزة حفظ الصوتيات بنجاح ✓**")
-
-@l313l.on(admin_cmd(pattern="(الصوتية تعطيل|صوتية تعطيل)"))
-async def disable_voice_save(event):
-    if gvarstatus("savevoiceforme"):
-        delgvar("savevoiceforme")
-        return await edit_delete(event, "**᯽︙تم تعطيل حفظ الصوتيات بنجاح ✓**")
-    else:
-        await edit_delete(event, "**᯽︙انت لم تفعل حفظ الصوتيات لتعطيلها!**")
-
-@l313l.on(admin_cmd(pattern="اكتب (.+)"))
-async def write_text_letter_by_letter(event):
-    text = event.pattern_match.group(1)
-    result = ""
-    for char in text:
-        result += char
-        await event.edit(result)
-        await asyncio.sleep(0.20)  # إضافة تأخير بسيط بين كل حرف وآخر
-
-@l313l.on(admin_cmd(pattern="تشغيل الاونلاين"))
-async def enable_online(event):
-    addgvar("always_online", "true")
-    await event.edit("**تم تفعيل ميزة البقاء أونلاين ✓**")
-    while gvarstatus("always_online"):
-        await bot.send_read_acknowledge(event.chat_id)
-        await asyncio.sleep(0)  # تكرار كل دقيقة
-
-@l313l.on(admin_cmd(pattern="ايقاف الاونلاين"))
-async def disable_online(event):
-    if gvarstatus("always_online"):
-        delgvar("always_online")
-        await event.edit("**تم تعطيل ميزة البقاء أونلاين ✓**")
-    else:
-        await event.edit("**ميزة البقاء أونلاين غير مفعلة!**")
 
 import openai
 
