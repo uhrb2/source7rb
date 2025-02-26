1e788d658ff7394b824b19294bb4c2025735ea69
uhrb2 <bhr288103@gmail.com>
2025-02-26 21:11:34 +0300

    Update تجارب.py

---
+++ JoKeRUB/plugins/تجارب.py
@@ -0,0 +1,9 @@
+from telethon import events
+import random, re
+from JoKeRUB.utils import admin_cmd
+import asyncio 
+from JoKeRUB import l313l
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+import os
+import datetime
+from JoKeRUB import *
