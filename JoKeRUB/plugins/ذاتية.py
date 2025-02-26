77ef5ffaf5e23244b99f40abdd9b7c72abcae8e7
uhrb2 <bhr288103@gmail.com>
2025-02-26 21:32:47 +0300

    Update ذاتية.py

--- JoKeRUB/plugins/ذاتية.py
+++ JoKeRUB/plugins/ذاتية.py
@@ -1,22 +1,5 @@
 
-@borg.on(
-    admin_cmd(pattern="لايك ?(.*)")
-)
-async def wspr(event):
-    if event.fwd_from:
-        return
-    l313lb = event.pattern_match.group(1)
-    rrrd7 = "@like"
-    if event.reply_to_msg_id:
-        reply_to_id = await event.get_reply_message()
-    tap = await bot.inline_query(rrrd7, l313lb) 
-    await tap[0].click(event.chat_id)
-    await event.delete()
 
-@borg.on(admin_cmd("م27"))
-async def _(event):
-     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("᯽︙ اوامر الهمسه واكس او \n\n⌔︙الامر  • `.همسة`\n⌔︙الاستخدام  • لكتابة همسه سرية لشخص في المجم[...]
 
 @borg.on(admin_cmd("الهمسة"))
 async def _(event):
