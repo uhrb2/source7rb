a1585ef8fbfb6d8670391772823b3938d50b0872
uhrb2 <bhr288103@gmail.com>
2025-02-28 10:48:52 +0300

    Update الاوامر.py

--- JoKeRUB/plugins/الاوامر.py
+++ JoKeRUB/plugins/الاوامر.py
@@ -26,7 +26,7 @@ async def _(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         F_O_1 = random.choice(rehu)
         await event.edit(
-            f"✦ **⦑ قائمة اوامر سورس {chr(128514)} Robin ⦒** ✦\n\n"
+            f"✦ **⦑ قائمة اوامر سورس \U0001F602 Robin ⦒** ✦\n\n"
             "1. **أوامر الادمن**\n"
             "   - `.م1`\n"
             "2. **أوامر المجموعة**\n"
