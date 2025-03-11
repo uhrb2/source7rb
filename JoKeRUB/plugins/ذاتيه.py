0280c0aedfae20a41107460d701e18e55dc0b0f4
uhrb2 <bhr288103@gmail.com>
2025-03-11 11:30:01 +0300

    Create ذاتيه.py

---
+++ JoKeRUB/plugins/ذاتيه.py
@@ -0,0 +1,66 @@
+from telethon import events
+import random, re
+from JoKeRUB.utils import admin_cmd
+import asyncio 
+from JoKeRUB import l313l
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+import os
+import datetime
+from JoKeRUB import *
+
+Aljoker_Asbo3 = {
+    'Monday': 'الاثنين',
+    'Tuesday': 'الثلاثاء',
+    'Wednesday': 'الأربعاء',
+    'Thursday': 'الخميس',
+    'Friday': 'الجمعة',
+    'Saturday': 'السبت',
+    'Sunday': 'الأحد'
+}
+
+@l313l.on(admin_cmd(pattern="(الذاتية تشغيل|ذاتية تشغيل)"))
+async def reda(event):
+    if gvarstatus ("savepicforme"):
+        return await edit_delete(event, "**᯽︙حفظ الذاتيات مفعل وليس بحاجة للتفعيل مجدداً **")
+    else:
+        addgvar("savepicforme", "reda")
+        await edit_delete(event, "**᯽︙تم تفعيل ميزة حفظ الذاتيات بنجاح ✓**")
+
+@l313l.on(admin_cmd(pattern="(الذاتية تعطيل|ذاتية تعطيل)"))
+async def Reda_Is_Here(event):
+    if gvarstatus ("savepicforme"):
+        delgvar("savepicforme")
+        return await edit_delete(event, "**᯽︙تم تعطيل حفظ الذاتيات بنجاح ✓**")
+    else:
+        await edit_delete(event, "**᯽︙انت لم تفعل حفظ الذاتيات لتعطيلها!**")
+
+def joker_unread_media(message):
+    return message.media_unread and (message.photo or message.video)
+
+async def Hussein(event, caption):
+    media = await event.download_media()
+    sender = await event.get_sender()
+    sender_id = event.sender_id
+    lMl10l_date = event.date.strftime("%Y-%m-%d")
+    lMl10l_day = Aljoker_Asbo3[event.date.strftime("%A")]
+    await bot.send_file(
+        "me",
+        media,
+        caption=caption.format(sender.first_name, sender_id, lMl10l_date, lMl10l_day),
+        parse_mode="markdown"
+    )
+    os.remove(media)
+
+@l313l.on(events.NewMessage(func=lambda e: e.is_private and joker_unread_media(e) and e.sender_id != bot.uid))
+async def Reda(event):
+    if gvarstatus("savepicforme"):
+        caption = """**
+           ♡    ♡
+♡ تم حفظ الذاتية بنجاح ✓
+♡ أسم المرسل : [{0}](tg://user?id={1})
+♡  تاريخ الذاتية : `{2}`
+♡  أرسلت في يوم `{3}`
+       ♡        ♡
+        **"""
+        await Hussein(event, caption)
+
