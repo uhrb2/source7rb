2f65390e1a90548578c2f8d24678187a08e02b90
uhrb2 <bhr288103@gmail.com>
2025-03-23 12:38:38 +0300

    Create صيد1.py

---
+++ JoKeRUB/plugins/صيد1.py
@@ -0,0 +1,349 @@
+import random
+import requests
+import time
+import asyncio
+from asyncio import sleep
+import telethon
+from telethon.sync import functions
+from telethon.errors import FloodWaitError
+from user_agent import generate_user_agent
+from JoKeRUB import l313l
+from ..core.managers import edit_or_reply
+
+trys = [0]
+crys = [0]
+arys = [0]
+brys = [0]
+itsclim = ["off"]
+iscuto = ["off"]
+istuto = ["off"]
+isbuto = ["off"]
+
+async def check_user(username):
+    url = "https://t.me/" + str(username)
+    headers = {
+        "User-Agent": generate_user_agent(),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7",
+    }
+
+    response = requests.get(url, headers=headers)
+    if (
+        response.text.find(
+            'If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"'
+        )
+        >= 0
+    ):
+        return True
+    else:
+        return False
+
+async def checker_user(username):
+    url = "https://t.me/" + str(username)
+    headers = {
+        "User-Agent": generate_user_agent(),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7",
+    }
+
+    response = requests.get(url, headers=headers)
+    if (
+        response.text.find(
+            'If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"'
+        )
+        >= 0
+    ):
+        return True
+    else:
+        return False
+
+async def gen_user(choice):
+    a = "qwertyuiopasdfghjklzxcvbnm"
+    b = "1234567890"
+    e = "qwertyuiopasdfghjklzxcvbnm1234567890"
+    z = "sdfghjklzwerty1234567890uioxcvbqpanm"
+    o = "0987654321"
+    q = "5432109876"
+    k = "mnbvcxzlkjhgfdsapoiuytrewq"
+    if choice == "سداسي_حرفين1": #ARAAAR
+        c = d = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], d[0], c[0], c[0], c[0], d[0]]
+        random.shuffle(f)
+        username = "".join(f)
+
+    elif choice == "سداسي_شرطه": #AAAA_R ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], c[0], c[0], "_", d[0]]
+        username = "".join(f)
+
+    elif choice == "سداسي_حرفين2": #AAAARR ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], c[0], c[0], d[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "سداسي_حرفين3": #AAARRA ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], c[0], d[0], d[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سداسي_حرفين4": #AARRAA ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], d[0], d[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سداسي_حرفين5": #ARRAAA ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], d[0], d[0], c[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سداسي_حرفين6": #AARRRR ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], d[0], d[0], d[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "ثلاثي1": #A_R_D
+        c = random.choices(a)
+        d = random.choices(e)
+        s = random.choices(z)
+        f = [c[0], "_", d[0], "_", s[0]]
+        username = "".join(f)
+
+    elif choice == "ثلاثي2": #A_7_R ~ 
+        c = random.choices(a)
+        d = random.choices(o)
+        s = random.choices(z)
+        f = [c[0], "_", d[0], "_", s[0]]
+        username = "".join(f)
+
+    elif choice == "ثلاثي3": #A_7_0 ~ 
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(o)
+        f = [c[0], "_", d[0], "_", s[0]]
+        username = "".join(f)
+
+    elif choice == "شبه رباعي1": #A_A_A_R ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", c[0], "_", c[0], "_", d[0]]
+        username = "".join(f)
+
+    elif choice == "شبه رباعي2": #A_R_R_R ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", d[0], "_", d[0], "_", d[0]]
+        username = "".join(f)
+
+    elif choice == "شبه رباعي3": #A_RR_A ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", d[0], d[0], "_", c[0]]
+        username = "".join(f)
+
+    elif choice == "شبه رباعي4": #A_RR_R ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", d[0], d[0], "_", d[0]]
+        username = "".join(f)
+    elif choice == "رباعي1": #AAA_R ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], c[0], c[0], "_", d[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي2": #A_RRR ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", d[0], d[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي3": #AA_RR ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], "_", d[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي4": #AA_AR ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], "_", c[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي5": #AA_RA ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], "_", d[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي6": #AR_RA ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], d[0], "_", d[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "رباعي7": #AR_AR ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], d[0], "_", c[0], d[0]]
+        username = "".join(f)
+
+
+    elif choice == "رباعي8": #AR_RR ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        f = [c[0], d[0], "_", d[0], d[0]]
+        username = "".join(f)
+
+
+    elif choice == "بوتات1": #AR_Bot ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], d[0], "_", "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات2": #A_RBot ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], "_", d[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات3": #AR7Bot ~ 
+        c = random.choices(a)
+        d = random.choices(k)
+        s = random.choices(b)
+        f = [c[0], d[0], s[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات4": #A7RBot ~ 
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(k)
+        f = [c[0], d[0], s[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات5": #A77Bot ~ 
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(o)
+        f = [c[0], d[0], s[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات6": #ADRBot
+        c = random.choices(a)
+        d = random.choices(e)
+        s = random.choices(z)
+        f = [c[0], d[0], s[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات7": #(AARBot - AA8bot) ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], d[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات8": #AARBot ~ 
+        c = random.choices(a)
+        d = random.choices(k)
+        f = [c[0], c[0], d[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "بوتات9": #AA8Bot ~ 
+        c = random.choices(a)
+        d = random.choices(o)
+        f = [c[0], c[0], d[0], "b", "o", "t"]
+        username = "".join(f)
+
+    elif choice == "خماسي حرفين1": #AAARD ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        s = random.choices(e)
+        f = [c[0], c[0], c[0], s[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "خماسي ارقام": #AR888 ~ 
+        c = random.choices(a)
+        d = random.choices(e)
+        s = random.choices(b)
+        f = [c[0], d[0], s[0], s[0], s[0]]
+        username = "".join(f)
+
+    elif choice == "خماسي حرفين2": #A7RRR ~ 
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(z)
+        f = [c[0], d[0], s[0], s[0], s[0]]
+        username = "".join(f)
+
+
+
+
+
+    elif choice == "خماسي حرفين3": #ARRRD ~ 
+        c = random.choices(a)
+        d = random.choices(z)
+        s = random.choices(e)
+        f = [c[0], d[0], d[0], d[0], s[0]]
+        username = "".join(f)
+
+
+    elif choice == "سباعيات1": #AAAAAAR ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], c[0], c[0], c[0], c[0], d[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات2": #AAAAARA ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], c[0], c[0], c[0], d[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات3": #AAAARAA
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], c[0], c[0], d[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات4": #AAARAAA ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], c[0], d[0], c[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات5": #AARAAAA ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], c[0], d[0], c[0], c[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات6": #ARAAAAA ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], d[0], c[0], c[0], c[0], c[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "سباعيات7": #ARRRRRR ~ 
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], d[0], d[0], d[0], d[0], d[0], d[0]]
+        username = "".join(f)
+    elif choice == "متاح":
+        c = d = random.choices(a)
+        d = random.choices(z)
+        f = [c[0], d[0], d[0], d[0], d[0], d[0], d[0], c[0], d[0], c[0]]
+        username = "".join(f)
+
+    elif choice == "ايقاف": #
+        return "stop"
+    else:
+        return "error"
+    return username
