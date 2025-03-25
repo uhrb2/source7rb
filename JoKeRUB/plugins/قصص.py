330079a6061a0694bffa2e9b74958116b51d40b1
uhrb2 <bhr288103@gmail.com>
2025-03-25 16:32:10 +0300

    Update قصص.py

--- JoKeRUB/plugins/قصص.py
+++ JoKeRUB/plugins/قصص.py
@@ -14,18 +14,23 @@ async def download_story(event):
     url = event.pattern_match.group(1)
     await event.edit("**᯽︙جاري تحميل القصة ...**")
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            if response.status == 200:
-                with open("story.mp4", "wb") as file:
-                    while True:
-                        chunk = await response.content.read(1024)
-                        if not chunk:
-                            break
-                        file.write(chunk)
-                
-                await event.client.send_file("me", "story.mp4")
-                await event.edit("**᯽︙تم تحميل القصة وإرسالها إلى الرسائل المحفوظة بنجاح ✓**")
-                os.remove("story.mp4")
-            else:
-                await event.edit("**᯽︙حدث خطأ أثناء تحميل القصة ✗**")
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    file_path = "story.mp4"
+                    with open(file_path, "wb") as file:
+                        while True:
+                            chunk = await response.content.read(1024)
+                            if not chunk:
+                                break
+                            file.write(chunk)
+
+                    await event.client.send_file("me", file_path)
+                    await event.edit("**᯽︙تم تحميل القصة وإرسالها إلى الرسائل المحفوظة بنجاح ✓**")
+                    os.remove(file_path)
+                else:
+                    await event.edit("**᯽︙حدث خطأ أثناء تحميل القصة ✗**")
+    except Exception as e:
+        await event.edit(f"**᯽︙حدث خطأ: {str(e)} ✗**")
+
