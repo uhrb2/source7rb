7a9f01adf1a6cdfd9ea33d2a7f667a96a896ab0d
uhrb2 <bhr288103@gmail.com>
2025-03-25 16:28:11 +0300

    Update تجارب.py

--- JoKeRUB/plugins/تجارب.py
+++ JoKeRUB/plugins/تجارب.py
@@ -7,7 +7,6 @@ from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 import os
 import datetime
 from JoKeRUB import *
-import aiohttp
 
 @l313l.on(admin_cmd(pattern="(الصوتية تشغيل|صوتية تشغيل)"))
 async def enable_voice_save(event):
@@ -33,25 +32,3 @@ async def write_text_letter_by_letter(event):
         result += char
         await event.edit(result)
         await asyncio.sleep(0.20)  # إضافة تأخير بسيط بين كل حرف وآخر
-
-
-@l313l.on(admin_cmd(pattern="تحميل قصة (.+)"))
-async def download_story(event):
-    url = event.pattern_match.group(1)
-    await event.edit("**᯽︙جاري تحميل القصة ...**")
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            if response.status == 200:
-                with open("story.mp4", "wb") as file:
-                    while True:
-                        chunk = await response.content.read(1024)
-                        if not chunk:
-                            break
-                        file.write(chunk)
-                
-                await event.client.send_file("me", "story.mp4")
-                await event.edit("**᯽︙تم تحميل القصة وإرسالها إلى الرسائل المحفوظة بنجاح ✓**")
-                os.remove("story.mp4")
-            else:
-                await event.edit("**᯽︙حدث خطأ أثناء تحميل القصة ✗**")
