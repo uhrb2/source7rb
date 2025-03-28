31322020072d5764b9a9a56a8635e7f3e3661477
uhrb2 <bhr288103@gmail.com>
2025-03-28 15:15:58 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -10,7 +10,7 @@ class Development(Config):
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
     STRING_SESSION = "1ApWapzMBu37ND8xcZCUJiNSytahxoRK7bnW4tq3mafBjpfFGCp_zUDQcIagdrcIqrqiS5TPjf06XGCcSBWhYpVuxMqc4K3Af62AsGrgYgZMdG04HLQMc03QDl0IwmvQO4hIqjBXGYUz9qDgo04hc6qRTtqvyZfUrqkPC4KWWbixvHR0inXCGAQJSctsGY1ZG5ecTov5o1hob4AwmydFOAJkRXs2czlzjcMAcPVX5luE8ZpqtnC6PC4Jf6EEq_K9FcsLehik_2K7I5mV4PPX4nxrgLWvMWHAiHGtQrAAvFgtZENEcSq2L2ZUHYRGXjJBakGemNWHPZ-vCFHuGChHvCAq0tI-gzfg="
     # create a new bot in @botfather and fill the following vales with bottoken and username respectively
-    TG_BOT_TOKEN = 8141239845:AAFx3tZ6b0crjWC0XDxz2as3-CyqzF0Nvqs"
+    TG_BOT_TOKEN = "8141239845:AAFx3tZ6b0crjWC0XDxz2as3-CyqzF0Nvqs"
     # command handler
     COMMAND_HAND_LER = "."
     # sudo enter the id of sudo users userid's in that array
