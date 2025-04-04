e82a43350ddd3d2d95e1976b0ac993895a6c8219
uhrb2 <bhr288103@gmail.com>
2025-04-04 12:30:33 +0300

    Create con.py

---
+++ JoKeRUB/assistant/con.py
@@ -0,0 +1,24 @@
+#    جميع الحقوق لمطوري سورس روبن حصريا لهم فقط
+#    اذا تخمط الملف اذك الحقوق وكاتبيه ومطوريه لا تحذف الحقوق وتصير فاشل 👍
+#    كتابة الشسد 
+import asyncio
+import io
+import re
+
+from telethon import Button, custom, events
+from telethon.tl.functions.users import GetFullUserRequest
+from JoKeRUB import bot
+from JoKeRUB.sql_helper.blacklist_assistant import (
+    add_nibba_in_db,
+    is_he_added,
+    removenibba,
+)
+from JoKeRUB.sql_helper.botusers_sql import add_me_in_db, his_userid
+from JoKeRUB.sql_helper.idadder_sql import (
+    add_usersid_in_db,
+    already_added,
+    get_all_users,
+)
+from l313l.razan.resources.assistant import *
+#start 
+@tgbot.on(events.NewMessage(pattern="^/con"))
