a1f86f749b26542e7456bc985f40d09324e8cea5
uhrb2 <bhr288103@gmail.com>
2025-04-04 17:09:40 +0300

    Update ادمن.py

--- JoKeRUB/plugins/ادمن.py
+++ JoKeRUB/plugins/ادمن.py
@@ -5,6 +5,26 @@ import asyncio
 import io
 import re
 
+import html
+import os
+import random
+import requests
+from requests import get
+from translate import Translator
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.utils import get_input_location
+from JoKeRUB import l313l
+from random import choice
+from l313l.razan.resources.strings import *
+from telethon import events
+from ..Config import Config
+from ..core.managers import edit_or_reply
+from ..helpers import get_user_from_event, reply_id
+from . import spamwatch
+from telethon.utils import get_display_name
+from ..helpers.utils import reply_id, _catutils, parse_pre, yaml_format, install_pip, get_user_from_event, _format
+import asyncio
 from telethon import Button, custom, events
 from telethon.tl.functions.users import GetFullUserRequest
 from JoKeRUB import bot
