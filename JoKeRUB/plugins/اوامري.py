14ab2036bcba1b6da9f2a09d3501d0d549e9880e
uhrb2 <bhr288103@gmail.com>
2025-04-27 11:59:45 +0300

    Update اوامري.py

--- JoKeRUB/plugins/اوامري.py
+++ JoKeRUB/plugins/اوامري.py
@@ -22,35 +22,35 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         await bot.get_me()
         if query.startswith("اوامري") and event.query.user_id == bot.uid:
             buttons = [
-                [Button.inline("☪︎ اوامر الادمن ☪︎", data="l313l0")],
+                [Button.inline("💥 اوامر الادمن", data="l313l0")],
                 [
-                    Button.inline("☪︎ اوامر البوت ☪︎", data="rozbot"),
-                    Button.inline("☪︎ الحساب ☪︎", data="Jmrz"),
-                    Button.inline("☪︎ المجموعات ☪︎", data="gro"),
+                    Button.inline("☢️ اوامر البوت", data="rozbot"),
+                    Button.inline("🛄 الحساب", data="Jmrz"),
+                    Button.inline("⚠️ المجموعات", data="gro"),
                 ],
                 [
-                    Button.inline("☪︎ الصيغ و الجهات ☪︎", data="sejrz"),
-                    Button.inline("☪︎ الحماية و تلكراف ☪︎", data="grrz"),
+                    Button.inline("📳 الصيغ و الجهات", data="sejrz"),
+                    Button.inline("⛔️ الحماية و تلكراف", data="grrz"),
                 ],
                 [
-                    Button.inline("☪︎ اوامر التسلية ☪︎", data="tslrzj"),
-                    Button.inline("☪︎ الترحيبات والردود ☪︎", data="r7brz"),
+                    Button.inline("💠 اوامر التسلية", data="tslrzj"),
+                    Button.inline("📧 الترحيبات والردود", data="r7brz"),
                 ],
                 [
-                    Button.inline("☪︎ اومر المساعدة ☪︎", data="krrznd"),
-                    Button.inline("☪︎ الملصقات وصور ☪︎", data="jrzst"),
+                    Button.inline("💡 اومر المساعدة", data="krrznd"),
+                    Button.inline("🔎 الملصقات وصور🔍", data="jrzst"),
                 ],
                 [
-                    Button.inline("☪︎ التكرار والتنظيف ☪︎", data="krrznd"),
-                    Button.inline("☪︎ الترفيه ☪︎", data="rfhrz"),
+                    Button.inline("💾 التكرار والتنظيف", data="krrznd"),
+                    Button.inline("📊 الترفيه", data="rfhrz"),
                 ],
                 [
-                    Button.inline("☪︎ التكرار والتنظيف ☪︎", data="iiers"),
-                    Button.inline("☪︎ الملصقات وصور ☪︎", data="jrzst"),
+                    Button.inline("🧰 التكرار والتنظيف", data="iiers"),
+                    Button.inline("🛡 الملصقات وصور", data="jrzst"),
                 ],
                 [
-                    Button.inline("☪︎ الأكستـرا ☪︎", data="iiers"),
-                    Button.inline("☪︎ الانتحال والتقليد ☪︎", data="uscuxrz"),
+                    Button.inline("⚗️ الأكستـرا ⚗️", data="iiers"),
+                    Button.inline("🔧 الانتحال والتقليد 🔧", data="uscuxrz"),
                 ],
             ]
             if JEP_IC and JEP_IC.endswith((".jpg", ".png", "gif", "mp4")):
