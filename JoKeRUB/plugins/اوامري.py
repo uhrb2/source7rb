7b054e73cc94d88efd299e36af8161d4fb649617
uhrb2 <bhr288103@gmail.com>
2025-04-27 12:05:08 +0300

    Update اوامري.py

--- JoKeRUB/plugins/اوامري.py
+++ JoKeRUB/plugins/اوامري.py
@@ -22,30 +22,22 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         await bot.get_me()
         if query.startswith("اوامري") and event.query.user_id == bot.uid:
             buttons = [
-    [
-        Button.inline("💥 اوامر الادمن", data="l313l0"),
-        Button.inline("☢️ اوامر البوت", data="rozbot"),
-        Button.inline("🛄 الحساب", data="Jmrz"),
-        Button.inline("⚠️ المجموعات", data="gro"),
-    ],
-    [
-        Button.inline("📳 الصيغ و الجهات", data="sejrz"),
-        Button.inline("⛔️ الحماية و تلكراف", data="grrz"),
-        Button.inline("💠 اوامر التسلية", data="tslrzj"),
-        Button.inline("📧 الترحيبات والردود", data="r7brz"),
-    ],
-    [
-        Button.inline("💡 اومر المساعدة", data="krrznd"),
-        Button.inline("🔎 الملصقات وصور🔍", data="jrzst"),
-        Button.inline("💾 التكرار والتنظيف", data="krrznd"),
-        Button.inline("📊 الترفيه", data="rfhrz"),
-    ],
-    [
-        Button.inline("🧰 التكرار والتنظيف", data="iiers"),
-        Button.inline("🛡 الملصقات وصور", data="jrzst"),
-        Button.inline("⚗️ الأكستـرا ⚗️", data="iiers"),
-        Button.inline("🔧 الانتحال والتقليد 🔧", data="uscuxrz"),
-    ],
+    [Button.inline("💥 اوامر الادمن", data="l313l0")],
+    [Button.inline("☢️ اوامر البوت", data="rozbot")],
+    [Button.inline("🛄 الحساب", data="Jmrz")],
+    [Button.inline("⚠️ المجموعات", data="gro")],
+    [Button.inline("📳 الصيغ و الجهات", data="sejrz")],
+    [Button.inline("⛔️ الحماية و تلكراف", data="grrz")],
+    [Button.inline("💠 اوامر التسلية", data="tslrzj")],
+    [Button.inline("📧 الترحيبات والردود", data="r7brz")],
+    [Button.inline("💡 اومر المساعدة", data="krrznd")],
+    [Button.inline("🔎 الملصقات وصور🔍", data="jrzst")],
+    [Button.inline("💾 التكرار والتنظيف", data="krrznd")],
+    [Button.inline("📊 الترفيه", data="rfhrz")],
+    [Button.inline("🧰 التكرار والتنظيف", data="iiers")],
+    [Button.inline("🛡 الملصقات وصور", data="jrzst")],
+    [Button.inline("⚗️ الأكستـرا ⚗️", data="iiers")],
+    [Button.inline("🔧 الانتحال والتقليد 🔧", data="uscuxrz")],
             ]
             if JEP_IC and JEP_IC.endswith((".jpg", ".png", "gif", "mp4")):
                 result = builder.photo(
