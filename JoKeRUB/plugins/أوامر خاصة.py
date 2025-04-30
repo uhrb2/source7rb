46c500920ae7403e4775c39b372cb6ca25d03adc
uhrb2 <bhr288103@gmail.com>
2025-04-30 14:07:48 +0300

    Update أوامر خاصة.py

--- JoKeRUB/plugins/أوامر خاصة.py
+++ JoKeRUB/plugins/أوامر خاصة.py
@@ -7,22 +7,3 @@ from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 import os
 import datetime
 from JoKeRUB import *
-
-@borg.on(
-    admin_cmd(pattern="لايك ?(.*)")
-)
-async def wspr(event):
-    if event.fwd_from:
-        return
-    l313lb = event.pattern_match.group(1)
-    rrrd7 = "@like"
-    if event.reply_to_msg_id:
-        reply_to_id = await event.get_reply_message()
-    tap = await bot.inline_query(rrrd7, l313lb) 
-    await tap[0].click(event.chat_id)
-    await event.delete()
-
-@borg.on(admin_cmd("م27"))
-async def _(event):
-     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("᯽︙ الأوامر المدفوعة\n\n⌔︙الامر  • `.تشغيل الرد التلقائي`\n⌔︙الاستخدام  • للعبة اول من يكتب [...]
