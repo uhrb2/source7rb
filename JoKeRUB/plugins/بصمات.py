12d33a8b171608a27ca3c12264aba3fd72b06ab6
uhrb2 <bhr288103@gmail.com>
2025-06-10 11:17:11 +0300

    Update بصمات.py

---
+++ JoKeRUB/plugins/بصمات.py
@@ -0,0 +1,20 @@
+import html
+import os
+import random
+from telethon.tl.types import EmojiStatusEmpty
+import requests
+from requests import get
+from translate import Translator
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.utils import get_input_location
+from JoKeRUB import l313l
+from random import choice
+from l313l.razan.resources.strings import *
+from telethon import events
+from ..Config import Config
+from ..core.managers import edit_or_reply
+from ..helpers import get_user_from_event, reply_id
+from . import spamwatch
+from telethon.utils import get_display_name
+from ..helpers.utils import reply_id, _catutils, parse_pre, yaml_format, install_pip, get_user_from_event, _format
