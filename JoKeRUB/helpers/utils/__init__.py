45ad7869f90e9e900cea70bac30da394b5eb845a
uhrb2 <bhr288103@gmail.com>
2025-06-25 14:15:43 +0300

    Update __init__.py

--- JoKeRUB/helpers/utils/__init__.py
+++ JoKeRUB/helpers/utils/__init__.py
@@ -5,7 +5,12 @@ flag = True
 check = 0
 while flag:
     try:
+        from . import format as _format
+        from . import tools as _cattools
+        from . import utils as _catutils
         from .events import *
+        from .format import *
+
         break
     except ModuleNotFoundError as e:
         install_pip(e.name)
